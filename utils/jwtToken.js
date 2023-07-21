8d363a4314348fb838a85aeb6fe353955307e7ae
Shubham <shubhamboss1251@gmail.com>
2023-07-22 02:44:25 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,6 +9,7 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: true
   };
 
   res.status(statusCode).cookie("ecom_tkn", ecom_tkn, options).json({
